53cbcbe3dbd00ead950078e66c54012d30fb954a
Nishant Chauhan <166284550+Clipperai@users.noreply.github.com>
2026-06-21 20:32:09 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,59 @@
+function home() {
+
+        window.location.href = "portfolio.html";
+
+}
+
+function about() {
+
+        window.location.href = "../pages/about.html";
+
+}
+
+function projects() {
+
+        window.location.href = "../pages/projects.html";
+
+}
+
+function gallery() {
+
+        window.location.href = "../pages/gallery.html";
+
+}
+
+function contact() {
+
+        window.location.href = "../pages/contact.html";
+
+}
+
+
+
+// Socials
+
+
+function github() {
+
+         window.open("https://github.com/Clipperai", "_blank");
+
+}
+
+
+function youtube() {
+
+         window.open("https://www.youtube.com/@pyPRO2026", "_blank");
+
+}
+
+function linkedin() {
+
+         window.open("https://www.linkedin.com/in/nishant-btech/", "_blank");
+
+}
+
+function instagram() {
+
+         window.open("https://www.instagram.com/", "_blank");
+
+}
