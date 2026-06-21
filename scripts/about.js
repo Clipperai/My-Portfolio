bb9bb6e27876c9236036147bd3dda401fae86c5d
Nishant Chauhan <166284550+Clipperai@users.noreply.github.com>
2026-06-21 20:44:41 +0530

    Add files via upload

---
+++ scripts/about.js
@@ -0,0 +1,35 @@
+function content() {
+
+         window.open("https://www.youtube.com/@pyPRO2026", "_blank");
+
+}
+
+function home() {
+
+        window.location.href = "../portfolio.html";
+
+}
+
+function gallery() {
+
+        window.location.href = "gallery.html";
+
+}
+
+function projects() {
+
+        window.location.href = "projects.html";
+
+}
+
+function about() {
+
+        window.location.href = "about.html";
+
+}
+
+function contact() {
+
+        window.location.href = "contact.html";
+
+}
---
+++ scripts/projects.js
@@ -0,0 +1,60 @@
+function home() {
+
+        window.location.href = "../portfolio.html";
+
+}
+
+function gallery() {
+
+        window.location.href = "gallery.html";
+
+}
+
+function projects() {
+
+        window.location.href = "projects.html";
+
+}
+
+function about() {
+
+        window.location.href = "about.html";
+
+}
+
+function contact() {
+
+        window.location.href = "contact.html";
+
+}
+
+
+
+// Socials
+
+
+function github() {
+
+         window.open("https://github.com/Clipperai", "_blank");
+
+}
+
+
+function content() {
+
+         window.open("https://www.youtube.com/@pyPRO2026", "_blank");
+
+}
+
+function linkedin() {
+
+         window.open("https://www.youtube.com/@pyPRO2026", "_blank");
+
+}
+
+function instagram() {
+
+         window.open("https://www.youtube.com/@pyPRO2026", "_blank");
+
+}
+
